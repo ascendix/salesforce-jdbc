d0803796b9cd8dfd2edab6810770a1288432096a
vyermakov <vyermakov@vyermakov.artgroup.local>
2018-01-11 17:46:43 +0300

    Bugfix: Error in BIRT studio Deta Set editor on "?" parameter in SOQL query.

---
+++ src/test/java/com/ascendix/jdbc/salesforce/SfTestCredentials.java
@@ -0,0 +1,8 @@
+package com.ascendix.jdbc.salesforce;
+
+public interface SfTestCredentials {
+
+    public final static String USER = "";
+    public final static String PASSWORD_AND_SECRET = "";
+    
+}

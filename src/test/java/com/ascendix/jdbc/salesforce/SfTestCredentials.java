489cae0da6d0b05e8e6f23a7bdd1be48c7b51f4d
vyermakov <vyermakov@vyermakov.artgroup.local>
2018-01-18 13:54:03 +0300

    Bugfix: Incorrect support of field types "Date", "DateTime", "Time", "Blob", "Boolean", "Byte".

--- src/test/java/com/ascendix/jdbc/salesforce/SfTestCredentials.java
+++
@@ -1,8 +0,0 @@
-package com.ascendix.jdbc.salesforce;
-
-public interface SfTestCredentials {
-
-    public final static String USER = "";
-    public final static String PASSWORD_AND_SECRET = "";
-    
-}

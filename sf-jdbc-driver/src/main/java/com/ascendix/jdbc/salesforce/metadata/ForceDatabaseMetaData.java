2bed34d397799de695d1a839ba7de8041633b5c8
Sergii Puliaiev <spuliaiev@salesforce.com>
2020-11-09 09:17:07 -0800

    1) Fields for SubSelects aliases 2) Returning flat resultset for field of sub-entities

--- sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/metadata/ForceDatabaseMetaData.java
+++ sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/metadata/ForceDatabaseMetaData.java
@@ -437,7 +437,7 @@ public class ForceDatabaseMetaData implements DatabaseMetaData, Serializable {
 
     @Override
     public String getDriverVersion() throws SQLException {
-        return "1.2.6";
+        return "1.2.6.02";
     }
 
     @Override
